3fb7a5f5a5fa4cd95d52d29fa8e494e99fe3832a
ezramarks <ezrablankmarks@gmail.com>
2020-04-27 16:46:26 -0400

    Clean up

--- code/flashing_video_processing.py
+++ code/flashing_video_processing.py
@@ -2,23 +2,22 @@ from threading import Thread
 from queue import Queue
 import numpy as np
 import cv2
-import time
 
 class ReadingThread(Thread):
-    def __init__(self, input_queue, filename):
+    def __init__(self, input_queue, video):
         Thread.__init__(self)
         self.name = "Reading Thread"
         self.input_queue = input_queue
-        self.filename = filename
+        self.video = video
     
     def run(self):
-        while video.isOpened():
+        while (self.video.isOpened()):
             # load frames into queue
-            ret, frame = video.read()
-            if(ret):
+            ret, frame = self.video.read()
+            if (ret):
                 self.input_queue.put(frame)
 
-        video.release()
+        self.video.release()
 
 
 class ProcessingThread(Thread):
@@ -37,25 +36,39 @@ class ProcessingThread(Thread):
 
         while (True):
             if (self.input_queue.qsize() >= num_frames):
-                # load frames into array -- pop these frames off of the input queue,
-                # since we will no longer need them
+                # loading frames into array for processing:
                 for i in range(num_frames - overlap):
+                    # pop these frames off of the input queue,
+                    # since we will no longer need them
                     frames[:, :, :, i] = self.input_queue.get()
-                # load remaining frames into array -- keep them in the input queue
-                # for reuse (overlap) when processing the next segment of the video
                 for i in range(overlap):
+                    # keep these frames in the input queue for reuse (overlap)
+                    # when processing the next segment of the video
                     frames[:, :, :, num_frames - overlap + i] = self.input_queue.queue[i]
                 
                 flash = detect_flashes(frames)
-                print(flash)
 
                 for i in range(num_frames - overlap):
                     frame = np.copy(frames[:, :, :, i])
                     if flash:
                         frame = frame // 10
                     self.output_queue.put(frame)
-                    
 
+
+class WritingThread(Thread):
+    def __init__(self, output_queue):
+        Thread.__init__(self)
+        self.name = "Writing Thread"
+        self.output_queue = output_queue
+    
+    def run(self):
+        while (True): # TODO close window when video is over
+            # read frames from queue
+            frame = self.output_queue.get()
+            cv2.imshow('frame', frame)
+            cv2.waitKey(33)
+        
+        cv2.destroyAllWindows()
 
 def detect_flashes(frames):
     resolution = 5
@@ -63,7 +76,6 @@ def detect_flashes(frames):
     # height and width of an image region in which to detect total brightness change
     window_h = int(frames.shape[0] / resolution)
     window_w = int(frames.shape[1] / resolution)
-
 
     # luminance change from one frame to the next
     lumen_changes = np.zeros((resolution, resolution, num_frames - 1), dtype=np.int32)
@@ -85,7 +97,7 @@ def detect_flashes(frames):
 
     # sum the amount that the brightness changes between all the
     # frames in this video segment -- if the brightness changes a lot,
-    # there are probably flashes during this segment ¯\_(ツ)_/¯
+    # there are likely flashes during this segment
     total_lumen_changes = np.sum(abs_lumen_changes, axis=2, dtype=np.int32)
 
     regional_flashes = total_lumen_changes > threshold
@@ -96,30 +108,13 @@ def detect_flashes(frames):
     return flash
 
 
-class WritingThread(Thread):
-    def __init__(self, output_queue):
-        Thread.__init__(self)
-        self.name = "Writing Thread"
-        self.output_queue = output_queue
-    
-    def run(self):
-        while (True):
-            # read frames from queue
-            print(self.output_queue.qsize())
-            frame = self.output_queue.get()
-            cv2.imshow('frame', frame)
-            cv2.waitKey(33)
-        
-        cv2.destroyAllWindows()
-
-
 # FIFO (first-in-first-out) queue to hold frames after reading them in
 input_queue = Queue()
 # FIFO queue to hold frames after processing, before writing/displaying them out
 output_queue = Queue()
 
 # create video reader
-video = cv2.VideoCapture("video720p.mp4")
+video = cv2.VideoCapture("video.mp4")
 if not video.isOpened():
     print("Error Opening Video File")
 
